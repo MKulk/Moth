59efa67d74b5bcf2fcbfb4cb70712d6ed1aa59cd
MKulk <32264069+MKulk@users.noreply.github.com>
2022-01-07 18:56:21 +0100

    Update CS.py

--- CS.py
+++ CS.py
@@ -9,9 +9,11 @@ MultilayerParameters={
             "CurieTemperature":                 (1050,      160,        1050),
             "GammaCoefficient":                 (0.86,      0.86,       0.86),
             "InitPosition":                     (80,        80,         80   ),
-            "LongRangeInteractionLength":       (0.3,       1,          0.3 ),
-            "LongRangeExchangeFlag":            False,
-            "PeriodicBoundaryConditions":       False
+            "LongRangeInteractionLength":       (0.15,       1,          0.15 ),
+            "LongRangeExchangeFlag":            True,
+            "PeriodicBoundaryConditions":       False,
+            "InitPositionSingle":               0.0,
+            "InitB":                            (0.75,      0.75,       0.75),
             }
 MaterialExchange={
             "Py-Py"     :0.23,
@@ -20,8 +22,8 @@ MaterialExchange={
             "FeCr-FeCr" :0.023
             }
 LongRangeExchange={
-            "FeCr2-FeCr1"  :-0.0018,
-            "FeCr1-FeCr2"  :-0.0018
+            "Py-FeCr"  :0.0005,
+            "FeCr-Py"  :0.0005
             }
 
 Hmin=-0.07#5
@@ -36,29 +38,34 @@ from CalculationClass import simulation, timeit
 from viewer import reader
 # ctrl+shift+P: Debug Visualizer: New View
 @timeit
-def f():
+@timeit
+def f(PathToFolder,StructureParameters,StructureExchange,LongRangeExchange):
+    Hmin=-0.1#338
+    Hmax=0.05#1338
+    Hsteps=32
+    Tmin=1#10
+    Tmax=300#400
+    Tsteps=16
     S=simulation(DeleteFlag=True,
-                DescendingCoefficient=2,
-                PathToFolder="CS short range interaction only test debug",
-                StructureParameters=MultilayerParameters,
-                StructureExchange=MaterialExchange,
-                NumberOfIterationM=40,
-                NumberOfIterationTheta=1,
-                NumberOfSteps=1200)
-
+                 DescendingCoefficient=2,
+                 PathToFolder=PathToFolder,
+                 StructureParameters=StructureParameters,
+                 StructureExchange=StructureExchange,
+                 LongRangeExchange=LongRangeExchange,
+                 NumberOfIterationM=50,
+                 NumberOfIterationTheta=1,
+                 NumberOfSteps=1000)
     S.mode(Debug=False)
     file=S.GetMHvsT(
-                Hmin=Hmin,
-                Hmax=Hmax,
-                Hsteps=Hsteps,
-                Tmin=Tmin,
-                Tmax=Tmax,
-                Tsteps=Tsteps,
-                FieldDirection=0)
+                    Hmin=Hmin,
+                    Hmax=Hmax,
+                    Hsteps=Hsteps,
+                    Tmin=Tmin,
+                    Tmax=Tmax,
+                    Tsteps=Tsteps,
+                    FieldDirection=0)
     data=reader(file)
     data.GetMHonT()
     data.GetMTonH()
 
-
-
-f()
+f(PathToFolder="saf test long range",StructureParameters=MultilayerParameters,StructureExchange=MaterialExchange,LongRangeExchange=LongRangeExchange)

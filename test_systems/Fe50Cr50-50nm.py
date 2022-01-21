b5ba214f70bab877aae4625cd5e9861160fab9c5
MKulk <32264069+MKulk@users.noreply.github.com>
2022-01-21 01:29:13 +0100

    separation of configs and core script

---
+++ test_systems/Fe50Cr50-50nm.py
@@ -0,0 +1,31 @@
+FolderName          =   "Fe50Cr50-50nm"
+Hmin,Hmax,Hsteps    =   0.1,      0.1,      1
+Tmin,Tmax,Tsteps    =   0,      450,    256
+NumberOfSteps       =   2400
+FieldDirection      =   0
+StructureParameters={
+            "MaterialThickness":            (50.0,   ),
+            "MLThickness":                  (0.15,  ),
+            "ZeemanThickness":              (1.0,   ),
+            "MaterialName":                 ("FeCr",),
+            "MaterialS":                    (1,     ),
+            "MaterialExtraField":           (0,     ),
+            "MaterialExtraFieldDirection":  (0,     ),
+            "MaterialSaturationM":          (778.5, ),
+            "CurieTemperature":             (0,     ),
+            "GammaCoefficient":             (0.86,  ),
+            "InitPosition":                 (0.0,   ),
+            "InitB":                        (1.0,   ),
+            "LongRangeInteractionLength":   (0.0,   ),
+            "LongRangeExchangeFlag":         False,
+            "InitPositionSingle":            0,
+            "PeriodicBoundaryConditions":    False
+            }
+MaterialExchange={
+            "FeCr-FeCr"   :0.029
+            }
+
+LongRangeExchange={
+            "FeCr2-FeCr1"  :-0.0008,
+            "FeCr1-FeCr2"  :-0.0008
+            }#not used here
--- test_systems/SAF-direct.py
+++ test_systems/SAF-direct.py
@@ -1,18 +1,22 @@
-
-SAFParameters5={
-            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2     ),
-            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15    ),
-            "ZeemanThickness":              (1.0,       1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0     ),
-            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",   "FeCr1"  ),
-            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1,          1       ),
-            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0,          0       ),
-            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0,          0       ),
-            "MaterialSaturationM":          (778.5,     778.5,      778.5,      778.5,      778.5,      778.5,      778.5,      778.5,      778.5   ),
-            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0,          0       ),
-            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86    ),
-            "InitPosition":                 (45.0,      -45.0,      45.0,       -45.0,      45.0,       -45.0,      45.0,       -45.0,      45.0    ),
-            "InitB":                        (0.75,      0.74,       0.75,       0.74,       0.75,       0.74,       0.75,       0.74,       0.75    ),
-            "LongRangeInteractionLength":   (0.45,      0.45,       0.45,       0.45,       0.45,       0.45,       0.45,       0.45,       0.45    ),
+FolderName          =   "Fe50Cr50-1.2nm"
+Hmin,Hmax,Hsteps    =   0.001,      0.5,      64
+Tmin,Tmax,Tsteps    =   284,      291,    32
+NumberOfSteps       =   10000
+FieldDirection      =   0
+StructureParameters={
+            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2     ),
+            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15    ),
+            "ZeemanThickness":              (1.0,       1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0     ),
+            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2" ),
+            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1       ),
+            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0       ),
+            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0       ),
+            "MaterialSaturationM":          (778.5,     778.5,      778.5,      778.5,      778.5,      778.5,      778.5,      778.5   ),
+            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0       ),
+            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86    ),
+            "InitPosition":                 (45.0,      -45.0,      45.0,       -45.0,      45.0,       -45.0,      45.0,       -45.0   ),
+            "InitB":                        (0.75,      0.74,       0.75,       0.74,       0.75,       0.74,       0.75,       0.74    ),
+            "LongRangeInteractionLength":   (0.45,      0.45,       0.45,       0.45,       0.45,       0.45,       0.45,       0.45    ),
             "LongRangeExchangeFlag":         False,
             "InitPositionSingle":            10,
             "PeriodicBoundaryConditions":    False
@@ -23,57 +27,8 @@ MaterialExchange={
             "FeCr1-FeCr2"   :-0.00145,
             "FeCr2-FeCr2"   :0.029
             }
-#Tc=300K gamma=0.045
-RKKYExchange={
+
+LongRangeExchange={
             "FeCr2-FeCr1"  :-0.0018,
             "FeCr1-FeCr2"  :-0.0018
-            }
-
-
-
-from CalculationClass import simulation, timeit
-from viewer import reader
-
-@timeit
-def f(PathToFolder,StructureParameters,StructureExchange,LongRangeExchange):
-    Hmin=0.0#338
-    Hmax=0.5#1338
-    Hsteps=64
-    Tmin=288 #10
-    Tmax=291#400
-    Tsteps=32
-    S=simulation(DeleteFlag=True,
-                 DescendingCoefficient=2,
-                 PathToFolder=PathToFolder,
-                 StructureParameters=StructureParameters,
-                 StructureExchange=StructureExchange,
-                 LongRangeExchange=LongRangeExchange,
-                 NumberOfIterationM=50,
-                 NumberOfIterationTheta=1,
-                 NumberOfSteps=24000)
-    S.mode(Debug=False)
-    file=S.GetMHvsT(
-                    Hmin=Hmin,
-                    Hmax=Hmax,
-                    Hsteps=Hsteps,
-                    Tmin=Tmin,
-                    Tmax=Tmax,
-                    Tsteps=Tsteps,
-                    FieldDirection=0)
-    data=reader(file)
-    data.GetMHonT()
-    data.GetMTonH()
-#no long range interaction
-#f(PathToFolder="SAF RKKY J=-0.0000 l=0.00 T=280-310",StructureParameters=SAFParameters0,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.15
-#f(PathToFolder="SAF RKKY J=-0.0018 l=0.15",StructureParameters=SAFParameters1,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.3
-#f(PathToFolder="SAF RKKY J=-0.0018 l=0.30 1-1",StructureParameters=SAFParameters2,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.45
-f(PathToFolder="SAF direct RKKY 18000 288-291 64x32",StructureParameters=SAFParameters5,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.6
-#f(PathToFolder="SAF RKKY J=-0.0018 l=0.60",StructureParameters=SAFParameters4,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
+            }#not used here

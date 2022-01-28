0f5ee457f20a57013946bd8942baaf0ec408b9f1
MKulk <32264069+MKulk@users.noreply.github.com>
2022-01-28 11:29:13 +0100

    Update SAF.py

--- test_systems/SAF.py
+++ test_systems/SAF.py
@@ -1,88 +1,10 @@
-SAFParameters0={
-            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2),
-            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15),
-            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2"),
-            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1),
-            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialSaturationM":          (519,       519,        519,        519,        519,        519,        519,        519),
-            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0),
-            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86),
-            "InitPosition":                 (2.0,       -2.0,       2.0,        -2.0,       2.0,        -2.0,       2.0,        -2.0),
-            "LongRangeInteractionLength":   (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15),
-            "LongRangeExchangeFlag":         False,
-            "InitPositionSingle":            10,
-            "PeriodicBoundaryConditions":    False
-            }
-SAFParameters1={
-            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2),
-            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15),
-            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2"),
-            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1),
-            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialSaturationM":          (519,       519,        519,        519,        519,        519,        519,        519),
-            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0),
-            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86),
-            "InitPosition":                 (80.0,      -80.0,      80.0,       -80.0,      80.0,       -80.0,      80.0,       -80.0),
-            "LongRangeInteractionLength":   (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15),
-            "LongRangeExchangeFlag":         True,
-            "InitPositionSingle":            10,
-            "PeriodicBoundaryConditions":    False
-            }
-
-SAFParameters2={
-            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2     ),
-            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15    ),
-            "ZeemanThickness":              (1.0,       1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0     ),
-            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",   "FeCr1"  ),
-            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1,          1       ),
-            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0,          0       ),
-            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0,          0       ),
-            "MaterialSaturationM":          (519,       519,        519,        519,        519,        519,        519,        519,        519     ),
-            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0,          0       ),
-            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86    ),
-            "InitPosition":                 (45.0,      -45.0,      45.0,       -45.0,      45.0,       -45.0,      45.0,       -45.0,      45.0    ),
-            "InitB":                        (0.75,      0.74,       0.75,       0.74,       0.75,       0.74,       0.75,       0.74,       0.75    ),
-            "LongRangeInteractionLength":   (0.3,       0.3,        0.3,        0.3,        0.3,        0.3,        0.3,        0.3,        0.3     ),
-            "LongRangeExchangeFlag":         True,
-            "InitPositionSingle":            10,
-            "PeriodicBoundaryConditions":    False
-            }
-SAFParameters3={
-            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2),
-            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15),
-            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2"),
-            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1),
-            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialSaturationM":          (519,       519,        519,        519,        519,        519,        519,        519),
-            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0),
-            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86),
-            "InitPosition":                 (45.0,      -45.0,      45.0,       -45.0,      45.0,       -45.0,      45.0,       -45.0),
-            "InitB":                        (0.75,      0.74,       0.75,       0.74,       0.74,       0.75,       0.74,       0.75),
-            "LongRangeInteractionLength":   (0.45,      0.45,       0.45,       0.45,       0.45,       0.45,       0.45,       0.45),
-            "LongRangeExchangeFlag":         True,
-            "InitPositionSingle":            10,
-            "PeriodicBoundaryConditions":    False
-            }
-SAFParameters4={
-            "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2),
-            "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15),
-            "MaterialName":                 ("FeCr1",   "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2",    "FeCr1",    "FeCr2"),
-            "MaterialS":                    (1,         1,          1,          1,          1,          1,          1,          1),
-            "MaterialExtraField":           (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialExtraFieldDirection":  (0,         0,          0,          0,          0,          0,          0,          0),
-            "MaterialSaturationM":          (519,       519,        519,        519,        519,        519,        519,        519),
-            "CurieTemperature":             (0,         0,          0,          0,          0,          0,          0,          0),
-            "GammaCoefficient":             (0.86,      0.86,       0.86,       0.86,       0.86,       0.86,       0.86,       0.86),
-            "InitPosition":                 (80.0,      -80.0,      80.0,       -80.0,      80.0,       -80.0,      80.0,       -80.0),
-            "LongRangeInteractionLength":   (0.6,       0.6,        0.6,        0.6,        0.6,        0.6,        0.6,        0.6),
-            "LongRangeExchangeFlag":         True,
-            "InitPositionSingle":            10,
-            "PeriodicBoundaryConditions":    False
-            }
-SAFParameters5={
+FolderName          =   "SAF-0.45nm-5proc"
+Hmin,Hmax,Hsteps    =   0.001,      0.5,      64
+Tmin,Tmax,Tsteps    =   274,      314,    128
+NumberOfSteps       =   24000
+FieldDirection      =   0
+Acceleration        =   1.0
+StructureParameters={
             "MaterialThickness":            (1.2,       1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2,        1.2     ),
             "MLThickness":                  (0.15,      0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15,       0.15    ),
             "ZeemanThickness":              (1.0,       1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0,        1.0     ),
@@ -106,57 +28,8 @@ MaterialExchange={
             "FeCr1-FeCr2"   :0.000,
             "FeCr2-FeCr2"   :0.045
             }
-#Tc=300K gamma=0.045
-RKKYExchange={
-            "FeCr2-FeCr1"  :-0.0018,
-            "FeCr1-FeCr2"  :-0.0018
+
+LongRangeExchange={
+            "Py-FeCr"  :-0.00225,
+            "FeCr-Py"  :-0.00225
             }
-
-
-
-from CalculationClass import simulation, timeit
-from viewer import reader
-
-@timeit
-def f(PathToFolder,StructureParameters,StructureExchange,LongRangeExchange):
-    Hmin=0.001#338
-    Hmax=1.0#1338
-    Hsteps=32
-    Tmin=10 #10
-    Tmax=10#400
-    Tsteps=1
-    S=simulation(DeleteFlag=True,
-                 DescendingCoefficient=2,
-                 PathToFolder=PathToFolder,
-                 StructureParameters=StructureParameters,
-                 StructureExchange=StructureExchange,
-                 LongRangeExchange=LongRangeExchange,
-                 NumberOfIterationM=50,
-                 NumberOfIterationTheta=1,
-                 NumberOfSteps=600)
-    S.mode(Debug=True)
-    file=S.GetMHvsT(
-                    Hmin=Hmin,
-                    Hmax=Hmax,
-                    Hsteps=Hsteps,
-                    Tmin=Tmin,
-                    Tmax=Tmax,
-                    Tsteps=Tsteps,
-                    FieldDirection=0)
-    data=reader(file)
-    data.GetMHonT()
-    data.GetMTonH()
-#no long range interaction
-#f(PathToFolder="SAF RKKY J=-0.0000 l=0.00 T=280-310",StructureParameters=SAFParameters0,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.15
-#f(PathToFolder="SAF RKKY J=-0.0018 l=0.15",StructureParameters=SAFParameters1,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.3
-#f(PathToFolder="SAF RKKY J=-0.0018 l=0.30 1-1",StructureParameters=SAFParameters2,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.45
-f(PathToFolder="x9 SAF RKKY J=-0.0018 l=0.45 ref2",StructureParameters=SAFParameters5,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
-
-#Long range exchange length=0.6
-#f(PathToFolder="SAF RKKY J=-0.0018 l=0.60",StructureParameters=SAFParameters4,StructureExchange=MaterialExchange,LongRangeExchange=RKKYExchange)
